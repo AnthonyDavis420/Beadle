ed6fca82b8bbd7edc44dbb07c6b0449d9fb68790
userKash <158263292+userKash@users.noreply.github.com>
2025-03-11 21:44:28 +0800

    cleaning codes

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,19 +5,15 @@ import { Appbar, Avatar } from "react-native-paper";
 const Header = () => {
   return (
     <Appbar.Header style={styles.header}>
-      {/* Greeting */}
       <View style={styles.titleContainer}>
         <Text style={styles.title}>
           Hello, <Text style={styles.blueText}>Beadle</Text> 👋
         </Text>
       </View>
-      {/* Replacing Search Icon with Image */}
       <Image
         source={require("../assets/images/Search.svg")}
         style={styles.image}
       />
-
-      {/* Profile Avatar */}
       <Image
         source={require("../assets/images/profile.svg")}
         style={styles.profile}
@@ -38,8 +34,8 @@ const styles = StyleSheet.create({
     fontWeight: "medium",
   },
   icon: {
-    width: 24, // Adjust width
-    height: 24, // Adjust height
+    width: 24,
+    height: 24,
     marginRight: 10, // Space between image and avatar
   },
   image: {
